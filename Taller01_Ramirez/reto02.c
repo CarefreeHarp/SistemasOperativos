4986e34680b564309661a4a238cc277f75d9438c
CarefreeHarp <daniramirezva@gmail.com>
2025-08-21 20:15:31 -0500

    Arreglo Reto 02

--- Taller01_Ramirez/reto02.c
+++ Taller01_Ramirez/reto02.c
@@ -21,7 +21,7 @@
 
 int main(){
 	char *q = NULL; //declara un puntero a un dato de tipo char y le asigna un nulo
-	printf("Solicitando espaco para 'Goodbye'\n"); // imprime un texto
+	printf("Solicitando espacio para 'Goodbye'\n"); // imprime un texto
 	q = (char*)malloc(strlen("Goodbye")+1); //reserva la cantidad de memoria que ocupe la cadena 'Goodbye' y le suma 1, ademas hace un casteo de que es una variable apuntadora a un tipo char
 
 	if(!q){ //verifica si la reserva de memoria fue exitosa
@@ -29,7 +29,7 @@ int main(){
 		exit(1);//termina el programa si no se pudo reservar la memoria
 	}
 
-	printf("A punto de copiar 'Goodbye' a q en la direccion %s\n", q);//imprime un texto
+	printf("A punto de copiar 'Goodbye' a q en la direccion: %p\n", q);//imprime un texto
 	strcpy(q, "Goodbye");// copia la cadena 'Goodbye' en q
 	printf("String copiado !!\n");//imprime un texto
 	printf("%s\n",q);//imprime el contenido de q osea la cadena 'Goodbye'
