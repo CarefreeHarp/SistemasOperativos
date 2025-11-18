b7668fcb8f774b275eaef1c5b7f6521cfa6a4d40
CarefreeHarp <daniramirezva@gmail.com>
2025-11-18 18:58:57 -0500

    Proyecto final

--- Proyecto/AgenteDeReservas.c
+++ Proyecto/AgenteDeReservas.c
@@ -1,21 +1,63 @@
-/*************************************************************************************
- * Pontificia Universidad Javeriana                                                  *
- * Fecha:
- * Autores: Daniel Felipe Ramirez Vargas - Guillermo Andrés Aponte Cárdenas          *
- * Materia: Sistemas Operativos                                                      *
- * Descripción: 
-*************************************************************************************/
+/***************************************************************
+ * Universidad: Pontificia Universidad Javeriana
+ * Carrera: Ingeniería de Sistemas
+ * Autor: Guillermo Aponte - Daniel Ramirez - David Tobar
+ * Materia: Sistemas Operativos
+ * Fecha: 16/11/2025
+ * Archivo: AgenteDeReservas.c
+ * Descripción:
+ *   Este módulo implementa la funcionalidad principal de un agente
+ *   dentro del sistema de reservas. Cada agente se encarga de leer
+ *   las solicitudes de reservas desde un archivo, procesarlas y 
+ *   comunicarse con el controlador mediante pipes para enviar 
+ *   peticiones y recibir respuestas de aceptación, reprogramación 
+ *   o rechazo. Además, los agentes esperan la señal de finalización 
+ *   del controlador utilizando un semáforo, asegurando que la 
+ *   ejecución de cada agente termine de manera coordinada. Este 
+ *   módulo también se encarga de manejar errores en la lectura de 
+ *   argumentos, apertura de semáforos y comunicación con pipes, 
+ *   garantizando la correcta liberación de memoria y la finalización
+ *   ordenada del programa.
+ ***************************************************************/
 
-#include <unistd.h> //librería para utilizar funciones como fork() y getpid()
-#include <pthread.h> //librería para manejo de hilos 
-#include <stdlib.h>// Para utilizar funciones como exit, perror y EXIT_FAILURE (manejo de errores)
-#include <fcntl.h>// Incluye la biblioteca para utilizar open y la bandera O_RDWR
-#include <stdio.h> //Librería para manejo de entradas y salidas
 #include "ModulosDeDefinicion/ModuloAgente.h" //Se incluye el modulo que contiene las declaraciones de las funciones y estructuras
 
+//Funcion principal
+int main(int argc, char *argv[]) {
 
-int main(){
-    
-    printf("Hola mundo Agente\n");
-    return 0;
+  //Se abre el semáforo que ya debe haber creado el controlador y se indican posibles errores
+  sem_t *fd_sem = sem_open("/terminarAgentes", 0);
+  if (fd_sem == SEM_FAILED) {
+    perror("Abriendo el semaforo");
+    return -1;
+  }
+
+  //Se declara una estructura para guardar y acomodar todos los argumentos que se le envíen al
+  //agente en su ejecución.
+  //Además se manejan posibles erres en la toma de argumentos
+  RetornoAgentes argumentos = tomarArgumentosAgente(argc, argv);
+  if (argumentos.retorno == -1) {
+    return -1;
+  }
+
+  //Se llama a la función que lee del archivo de solicitudes y se manejan posibles errores que retorne esta función
+  if (leerArchivo(argumentos) == -1) {
+    return -1;
+  }
+
+  //Una vez el semáforo acabe, va a hacer wait en el semáforo que creó el controlador.
+  //Dicho controlador inicia en 0 por lo cual todos los agentes quedan bloqueados en este punto
+  //hasta que el controlador hace los "post" necesarios para terminar la ejecución de todos
+  sem_wait(fd_sem);
+
+  //Se imprime el mensaje solicitado en el enunciado
+  printf("AGENTE %s TERMINA.\n", argumentos.nombre);
+
+  //Se libera memoria de todos los datos que necesitaban una reserva
+  free(argumentos.nombre);
+  free(argumentos.fileSolicitud);
+  free(argumentos.pipeRecibe);
+
+  //Devolvemos 0 para indicar la correcta ejecución del programa
+  return 0;
 }
